b939f8871a2ef3bdc9cd82148114752a19a9d6ad
kjanuda <janudakodi@gmail.com>
2026-03-27 12:00:14 +0530

    fix: TypeScript null error in Intersectionere)

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin:      'https://axentrajk.netlify.app/',
+    origin:      'https://axentrajk.netlify.app',
     credentials: true,
   });
 
